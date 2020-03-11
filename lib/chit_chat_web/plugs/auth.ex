af2bf56b018ab96f1f474b28086f1813e12dfb90
Medo <medo.a.haleem@gmail.com>
2020-03-11 12:56:15 +0200

    create Auth Plug

--- lib/chit_chat_web/plugs/auth.ex
+++ lib/chit_chat_web/plugs/auth.ex
@@ -2,13 +2,14 @@ defmodule ChitChat.Auth do
   import Plug.Conn
   import Phoenix.Controller
   alias ChitChatWeb.Router.Helpers, as: Routes
+  alias ChitChatWeb.ErrorView
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     user = user_id && ChitChat.Accounts.get_user!(user_id)
-    assign(conn, :current_user, user)
+    put_current_user(conn, user)
   end
 
   def logged_in_user(conn, _opts) do
@@ -20,5 +21,21 @@ defmodule ChitChat.Auth do
       |> redirect(to: Routes.page_path(conn, :index))
       |> halt()
     end
+  end
+
+
+  def admin_user(conn = %{assigns: %{admin_user: true}}, _), do: conn
+  def admin_user(conn, opts) do
+    if opts[:pokerface] do
+      conn |> put_status(404) |> render(ErrorView, :"404", message: "Page not found") |> halt()
+    end
+
+    conn |> put_flash(:error, "You don't have access to that page") |> redirect(to: Routes.page_path(conn, :index)) |> halt()
+  end
+
+  def put_current_user(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> assign(:admin_user, !!user && !!user.credential && user.credential.email == "test@test.com")
   end
 end
